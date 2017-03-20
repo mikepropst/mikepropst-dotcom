9723a86b727edd487f93b06b3c0ef0f91a6ecfbb
mike <mikepropst@gmail.com>
2017-03-20 12:19:30 -0700

    audio projects and CSS to rebulid audio page

--- site/accounts/mike.php
+++ site/accounts/mike.php
@@ -8,11 +8,10 @@ language: en
 role: admin
 token: 17b3d763b2677d8a32e6296a979e45547396eae9
 history:
-  - >
-    blog/so-long-and-thanks-for-all-the-lone-star-tallboys
+  - audio-projects/an-empty-room-unbirth-ep
+  - audio-projects/you-might-think-we-re-sharks-summer-moon
+  - blog/so-long-and-thanks-for-all-the-lone-star-tallboys
   - blog/my-favorite-recent-mixing-plugins
   - blog/mainstream-success-is-super-imminent
-  - blog/045-tour-of-jfk-s-twa-terminal
-  - blog/smokin
 firstname: ""
 lastname: ""
--- site/templates/audio-projects.php
+++ site/templates/audio-projects.php
@@ -4,17 +4,22 @@
 	<div class="main">
 		<div class="full-content">
 			<article>
-				<h2>Audio Project Portfolio</h2>
-				<?php echo $page->text()->html() ?>
-				<div class="audio-projects">
+				<h2 class="work__title">Audio Project Portfolio</h2>
+				<h3 class="work__subtitle"><?php echo $page->text()->html() ?></h3>
+				<div class="projects-list">
+
 			    <?php foreach(page('audio-projects')->children()->visible() as $proj): ?>
 
 		        	<div class="audio-project">
+
+								<h4><?php echo html($proj->artist()) ?>: <span style="font-weight: normal"><?php echo html($proj->projectTitle()) ?></span></h4>
+
 								<?php if ($proj->images() !=""): ?>
 									<div class="audio-project__image"><?php echo thumb($proj->images()->first(),array('width'=>200,'height'=>200,'crop'=>true)) ?></div>
 								<?php endif ?>
+
 								<div class="audio-project__info">
-									<h4><?php echo html($proj->artist()) ?>: <span style="font-weight: normal"><?php echo html($proj->projectTitle()) ?></span></h4>
+
 									<ul class="audio-project__tags">
 									  <?php foreach($proj->role()->split() as $role): ?>
 									  <li><?php echo $role ?></li>
