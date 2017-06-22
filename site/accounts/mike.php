5ad8bea57469105f03ae06ee17fcf320e6cd5209
mike <mikepropst@gmail.com>
2017-06-22 10:39:21 -0700

    D&B Credit writeup changes

--- site/accounts/mike.php
+++ site/accounts/mike.php
@@ -8,10 +8,10 @@ language: en
 role: admin
 token: 17b3d763b2677d8a32e6296a979e45547396eae9
 history:
+  - projects/d-b-credit
   - blog/juicero-teardown
   - blog/switching-to-studio-one
   - audio-projects/an-empty-room-unbirth-ep
   - audio-projects/you-might-think-we-re-sharks-summer-moon
-  - blog/so-long-and-thanks-for-all-the-lone-star-tallboys
 firstname: ""
 lastname: ""
